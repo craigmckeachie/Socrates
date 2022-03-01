2e9fee1062b4fbf73caa472797caa821de26da61
Craig McKeachie <craigdmckeachie@gmail.com>
2022-03-01 08:44:36 -0500

    adding model test, work in progress

---
+++ Socrates.Tests/ViewModels/EditCourseViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Socrates.Controllers;
+using System;
+using System.Collections.Specialized;
+using System.Web.Mvc;
+
+namespace Socrates.Tests.ViewModels
+{
+    [TestClass]
+    public class EditCourseViewModelTests
+    {
+        [TestMethod]
+        public void ModelBindingVerification()
+        {
+
+            var collection = new NameValueCollection();
+            collection.Add("Duration", "invalid string");
+            FormCollection formCollection = new FormCollection(collection);
+
+            var controller = new CourseController();
+            // CourseController.TryUpdateModel
+            
+
+        }
+    }
+}
